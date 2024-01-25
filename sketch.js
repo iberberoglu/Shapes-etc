e3fafd443fde426eef7beecf9aec4cfeb6516752
İsmail Berberoğlu <iberberoglu1@gmail.com>
2024-01-25 22:23:59 +0300

    clean code for sketch.js

--- sketch.js
+++ sketch.js
@@ -1,112 +1,90 @@
-let circleX, circleY;
-let r, g, b;
-let circleWidth = 40;
-let circleWidthRandom;
-let randomX;
-let randomY;
-let randomShape;
-let triangleXone,
-  triangleYone,
-  triangleXtwo,
-  triangleYtwo,
-  triangleXthree,
-  triangleYthree;
-let triangleRandomXone = 100;
-let triangleRandomYone = 100;
-let triangleRandomXtwo = 100;
-let triangleRandomYtwo = 100;
-let triangleRandomXthree = 100;
-let triangleRandomYthree = 100;
+let shapeX, shapeY;
+let shapeSize, shapeColor;
+let baseShapeSize = 40;
+let randomSize;
+let randomX, randomY;
+let randomShapeType;
+let triangleX1, triangleY1, triangleX2, triangleY2, triangleX3, triangleY3;
+let triangleBaseX = 100;
+let triangleBaseY = 100;
 let maxLength;
-let triangleButton = 50;
-let circleButton = 50;
+let enlargeButton = 50;
+let shrinkButton = 50;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
-  circleX = 0;
-  circleY = 0;
-  
+  shapeX = 0;
+  shapeY = 0;
 }
-/*
-function mousePressed() {
-  if(mouseX > (windowWidth - 100) && mouseY > (windowHeight - 100)) {
-    circleButton += 10;
-  }
-  if(mouseX > (windowWidth - 200) && mouseX < (windowWidth - 100) && mouseY > (windowHeight - 100)) {
-    circleButton -= 10;
-  }
-}
-*/
 
 function touchStarted() {
-  if(mouseX > (windowWidth - 100) && mouseY > (windowHeight - 100)) {
-    circleButton += 10;
+  if (mouseX > windowWidth - 100 && mouseY > windowHeight - 100) {
+    enlargeButton += 10;
   }
-  if(mouseX > (windowWidth - 200) && mouseX < (windowWidth - 100) && mouseY > (windowHeight - 100)) {
-    circleButton -= 10;
+  if (
+    mouseX > windowWidth - 200 &&
+    mouseX < windowWidth - 100 &&
+    mouseY > windowHeight - 100
+  ) {
+    enlargeButton -= 10;
   }
-  
 }
 
 function draw() {
-  if(mouseIsPressed) {
-    if(mouseX > (windowWidth - 100) && mouseY > (windowHeight - 100)) {
-      circleButton += 1;
-    } else if(mouseX > (windowWidth - 200) && mouseX < (windowWidth - 100) && mouseY > (windowHeight - 100)) {
-      circleButton -= 1;
+  if (mouseIsPressed) {
+    if (mouseX > windowWidth - 100 && mouseY > windowHeight - 100) {
+      enlargeButton += 1;
+    } else if (
+      mouseX > windowWidth - 200 &&
+      mouseX < windowWidth - 100 &&
+      mouseY > windowHeight - 100
+    ) {
+      enlargeButton -= 1;
     }
   }
-  fill(r, g, b);
+
+  shapeColor = color(random(255), random(255), random(255));
+  fill(shapeColor);
   rectMode(CENTER);
-  circleWidthRandom = random(circleButton);
-  circleWidth = random(1, circleWidthRandom);
-  r = random(255);
-  g = random(255);
-  b = random(255);
+  randomSize = random(enlargeButton);
+  shapeSize = random(1, randomSize);
 
-  circleX = random(windowWidth);
-  circleY = random(windowHeight);
+  shapeX = random(windowWidth);
+  shapeY = random(windowHeight);
 
-  maxLength = circleButton;
-  triangleXone = random(windowWidth);
-  triangleYone = random(windowHeight);
+  maxLength = enlargeButton;
+  triangleX1 = random(windowWidth);
+  triangleY1 = random(windowHeight);
 
-  triangleXtwo = triangleXone + random(-maxLength, maxLength);
-  triangleYtwo = triangleYone + random(-maxLength, maxLength);
-  triangleXthree = triangleXone + random(-maxLength, maxLength);
-  triangleYthree = triangleYone + random(-maxLength, maxLength);
+  triangleX2 = triangleX1 + random(-maxLength, maxLength);
+  triangleY2 = triangleY1 + random(-maxLength, maxLength);
+  triangleX3 = triangleX1 + random(-maxLength, maxLength);
+  triangleY3 = triangleY1 + random(-maxLength, maxLength);
 
-  let side1 = dist(triangleXone, triangleYone, triangleXtwo, triangleYtwo);
-  let side2 = dist(triangleXtwo, triangleYtwo, triangleXthree, triangleYthree);
-  let side3 = dist(triangleXthree, triangleYthree, triangleXone, triangleYone);
+  let side1 = dist(triangleX1, triangleY1, triangleX2, triangleY2);
+  let side2 = dist(triangleX2, triangleY2, triangleX3, triangleY3);
+  let side3 = dist(triangleX3, triangleY3, triangleX1, triangleY1);
 
-  randomShape = random(300);
+  randomShapeType = random(300);
 
-  if (randomShape > 0 && randomShape <= 100) {
-    circle(circleX, circleY, circleWidth);
-  } else if (randomShape > 100 && randomShape <= 200) {
-    rect(circleX, circleY, circleWidth);
+  if (randomShapeType > 0 && randomShapeType <= 100) {
+    circle(shapeX, shapeY, shapeSize);
+  } else if (randomShapeType > 100 && randomShapeType <= 200) {
+    rect(shapeX, shapeY, shapeSize);
   } else if (side1 < maxLength && side2 < maxLength && side3 < maxLength) {
-    triangle(
-      triangleXone,
-      triangleYone,
-      triangleXtwo,
-      triangleYtwo,
-      triangleXthree,
-      triangleYthree
-    );
+    triangle(triangleX1, triangleY1, triangleX2, triangleY2, triangleX3, triangleY3);
   }
+
   fill(255);
   let buttonX = windowWidth - 100;
   let buttonY = windowHeight - 50;
   rect(buttonX, buttonY, 200, 100, 25);
-  line((windowWidth - 100),(windowHeight - 100), (windowWidth - 100), windowHeight);
+  line(windowWidth - 100, windowHeight - 100, windowWidth - 100, windowHeight);
   fill(0);
-  textSize(5.5);
-  text("Şekilleri büyütmek için buraya tıkla", (windowWidth - 90), (windowHeight -50));
-  text("Şekilleri küçültmek için buraya tıkla", (windowWidth - 190), (windowHeight -50));
-  textSize(10);
-  text(circleButton, (windowWidth - 30), (windowHeight -70));
-  
+  textSize(12);
+  text("Bigger shapes", windowWidth - 90, windowHeight - 50);
+  text("Smaller shapes", windowWidth - 190, windowHeight - 50);
+  textSize(18);
+  text(enlargeButton, windowWidth - 30, windowHeight - 70);
 }
